3b0f1dfc847f963ca080d6546b5dcc0490f1c6ed
it27427 <istiak.tushar.qtec@gmail.com>
2022-07-24 10:17:56 +0600

    using typescript in vue 3 project

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,38 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div id="app">
+    <p>Name is: {{ name }} and Age is: {{ age }}</p>
+    <button @click="changeName('Istiak')" class="btn-change" type="button">Change Name</button>
+    <button @click="changeAge(30)" class="btn-change" type="button">Change Age</button>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+
+type numberOrString = number | string
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    
+  },
+  data() {
+    return {
+      name: 'Tushar' as string,
+      age: 29 as numberOrString
+    }
+  },
+  methods: {
+    changeName(name: string) {
+      this.name = name
+    },
+    changeAge(age: numberOrString) {
+      this.age = age
+    }
   }
 });
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+// IMPORT SCSS
+import './assets/scss/style.scss'
+
 createApp(App).mount('#app')
